8824dc58a07ffcab0186b4d3b40d70f907a33ea8
venom <hs4486952@gmail.com>
2024-07-30 10:14:00 +0530

    solve bugs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,43 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SquareComponent from './squareCompo';
 
 const TicTacToe = () => {
+     const[won,setWon] = useState(false);
      const[element,setElement] = useState(Array(9).fill(null))
      const[currTurn, setCurrTurn] = useState('x');
-     const winningCriteria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+     const winningCriteria = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]; 
+     const[winner,setWinner] = useState(null);
 
      const changeTurn = (e) => {
-       const newElement = [...element];
+        if(!won){
+          const newElement = [...element];
+          if(newElement[parseInt(e.target.id)] !== null){
+          return
+         } 
          newElement[parseInt(e.target.id)] = currTurn;
          setElement(newElement)
-         setCurrTurn(currEle => currEle === 'x' ? 'o' : 'x');    
+         setCurrTurn(currEle => currEle === 'x' ? 'o' : 'x');   
+        }    
      }
 
-     const winner = () => {
-        for(const win of winningCriteria){
-          const[a,b,c] = win;
-          if(element[a]!=null && (element[a]==element[b] && element[a]==element[c])){
-            return element[a];
+     useEffect(() => {
+      const checkWinner = () => {
+        for (const win of winningCriteria) {
+          const [a, b, c] = win;
+          if (element[a] !== null && element[a] === element[b] && element[a] === element[c]) {
+            setWon(true);
+            setWinner(element[a]);
+            return;
           }
         }
-     }
-     const checkWinner = winner();
+      };
+      checkWinner();
+    }, [element]);
 
      const reStart = () => {
         setElement(Array(9).fill(null));
         setCurrTurn('x');
+        setWon(false);
+        setWinner(null)
      }
 
     return (
       <>
-       { checkWinner ?
-       <div className="text-center"> 
-          <p className=" font-bold text-green-700 text-xl "> {checkWinner} Won the match </p> 
-          <button className="border w-24 h-8 bg-black text-white rounded" onClick={reStart}> play again </button>
-       </div>
-      
-        :  
-       
+         <h1 className="text-center font-bold text-red-500 text-xl"> {currTurn}'s Turn </h1>
        <div className="text-center my-5">
           <div className="flex justify-center">
             <SquareComponent changeTurn={changeTurn}  value={element[0]} id={0} />
@@ -56,8 +62,14 @@ const TicTacToe = () => {
             <SquareComponent changeTurn={changeTurn} value={element[7]}  id={7} />
             <SquareComponent changeTurn={changeTurn} value={element[8]}  id={8} />
           </div>
-        </div> }
-        
+        </div> 
+
+        { won && (
+           <div className="flex justify-center items-center"> 
+           <span className="text-green-600 font-semibold text-xl mx-3"> {winner}'s won the match </span> 
+            <button className="w-24 rounded-lg h-8 bg-black text-white" onClick={reStart}> Restart </button> 
+          </div> 
+        ) }
       </>
        
     )
